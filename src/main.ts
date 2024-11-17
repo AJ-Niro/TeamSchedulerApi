d983dcae59a349b786e491b822bffe9aafd98432
AndresJoaquinNino <andresninorodil@gmail.com>
2024-11-17 17:59:08 -0400

    style(main.ts): change || for ?? in PORT variable

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import UserController from './controllers/user.controller';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const userController = new UserController();
 app.use('/user', userController.router);
