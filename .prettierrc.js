e15383e898082438e70299284ebd2c989d493f96
AndresJoaquinNino <andresninorodil@gmail.com>
2024-11-18 10:32:05 -0400

    chore: basic prettier setup

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  printWidth: 80,
+  trailingComma: 'all',
+}
+
+export default config
